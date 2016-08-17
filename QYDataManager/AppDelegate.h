0aa23ac9fb119081e5411adae59be9ced6c7787e
shuzhongming <shuzhongming@qiyi.com>
2016-08-17 17:56:27 +0800

    Initial Commit

---
+++ QYDataManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYDataManager
+//
+//  Created by 舒小明 on 16/8/17.
+//  Copyright © 2016年 舒小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYDataManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYDataManager
+//
+//  Created by 舒小明 on 16/8/17.
+//  Copyright © 2016年 舒小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
